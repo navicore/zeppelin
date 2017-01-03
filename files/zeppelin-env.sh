e74216a83310c22bb8021b882eaf776dbf5ef95b
Ed Sweeney <ed@table.co>
2017-01-02 18:15:12 -0800

    debugging

--- files/zeppelin-env.sh
+++ files/zeppelin-env.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-SERVICE=${SPARK_SERVICE_NAME:-spark-master}
-SERVICE_PORT=${SPARK_SERVICE_PORT:-7077}
-PORT=${UI_PORT:-8080}
+# SERVICE=${SPARK_SERVICE_NAME:-spark-master}
+# SERVICE_PORT=${SPARK_SERVICE_PORT:-7077}
+# PORT=${UI_PORT:-8080}
+#
+# export MASTER="spark://${SERVICE}:${SERVICE_PORT}"
+# export SPARK_HOME=/opt/spark
+# export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
+# export ZEPPELIN_MEM=-Xmx1024m
+# export ZEPPELIN_PORT=${PORT}
+# export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.9-src.zip"
 
-export MASTER="spark://${SERVICE}:${SERVICE_PORT}"
+export MASTER="spark://spark-master:7077"
 export SPARK_HOME=/opt/spark
 export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 export ZEPPELIN_MEM=-Xmx1024m
-export ZEPPELIN_PORT=${PORT}
+export ZEPPELIN_PORT=8080
 export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.9-src.zip"
 
 echo "settings: master: $MASTER port: $ZEPPELIN_PORT"
