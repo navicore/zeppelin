4de914ac08b62ed9cb3139ebbd483f0e62955755
Ed Sweeney <ed@table.co>
2017-01-02 16:06:20 -0800

    override defaults

--- files/zeppelin-env.sh
+++ files/zeppelin-env.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export MASTER="spark://spark-master:7077"
+SERVICE=${SPARK_SERVICE_NAME:-spark-master}
+SERVICE_PORT=${SPARK_MASTER_PORT:-7077}
+PORT=${UI_PORT:-8080}
+
+export MASTER="spark://${SERVICE}:${SERVICE_PORT}"
 export SPARK_HOME=/opt/spark
 export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 export ZEPPELIN_MEM=-Xmx1024m
-export ZEPPELIN_PORT=8080
+export ZEPPELIN_PORT=${UI_PORT}
 export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.9-src.zip"
 
