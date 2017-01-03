adb496a24bfc9d5790fe7dc87b314213f69f4f76
Ed Sweeney <ed@table.co>
2017-01-02 16:24:50 -0800

    fix typo in ui port

--- files/zeppelin-env.sh
+++ files/zeppelin-env.sh
@@ -8,6 +8,6 @@ export MASTER="spark://${SERVICE}:${SERVICE_PORT}"
 export SPARK_HOME=/opt/spark
 export ZEPPELIN_NOTEBOOK_DIR="${ZEPPELIN_HOME}/notebook"
 export ZEPPELIN_MEM=-Xmx1024m
-export ZEPPELIN_PORT=${UI_PORT}
+export ZEPPELIN_PORT=${PORT}
 export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.9-src.zip"
 
